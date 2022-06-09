369d02da15444ae02b900656d561a9c3f2298925
IAGanja <abichaika@yandex.ru>
2022-06-09 18:42:53 +0300

    start: make spring initializr project

---
+++ src/main/java/com/tochitopor/ExchToGIF/ExchToGifApplication.java
@@ -0,0 +1,13 @@
+package com.tochitopor.ExchToGIF;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchToGifApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExchToGifApplication.class, args);
+	}
+
+}
